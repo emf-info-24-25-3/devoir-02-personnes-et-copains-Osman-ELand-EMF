e96e0abfc8f2aef511ea4304dab56820fd92b471
ELand osman <eland.osman@studentfr.ch>
2025-03-13 09:16:32 +0100

    fini

--- src/models/Personne.java
+++ src/models/Personne.java
@@ -1,17 +1,59 @@
 package models;
 
-public class Personne {
-    
-        //
-        //              )        (                 ) (           (         (
-        //           ( /(   *   ))\ )        (  ( /( )\ )        )\ )  (   )\ )
-        //     (   ( )\())` )  /(()/((       )\ )\()|()/(  (    (()/(  )\ (()/(
-        //     )\  )((_)\  ( )(_))(_))\    (((_|(_)\ /(_)) )\    /(_)|((_) /(_))
-        //    ((_)((_)((_)(_(_()|_))((_)   )\___ ((_|_))_ ((_)  (_)) )\___(_))
-        //    __   _____ _____ ___ ___    ___ ___  ___  ___   ___ ___ ___   _
-        //    \ \ / / _ \_   _| _ \ __|  / __/ _ \|   \| __| |_ _/ __|_ _| | |
-        //     \ V / (_) || | |   / _|  | (_| (_) | |) | _|   | | (__ | |  |_|
-        //      \_/ \___/ |_| |_|_\___|  \___\___/|___/|___| |___\___|___| (_)
-        //
+import java.util.ArrayList;
+import java.util.List;
 
+public class Personne {
+   public static final int MAX_COPAINS = 10;
+
+    private final String nom;
+    private final String prenom; 
+    private final List<Personne> copains;
+
+    public Personne(String nom, String prenom) {
+        this.nom = nom;
+        this.prenom = prenom;
+        this.copains = new ArrayList<>();
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public boolean ajouterCopain(Personne nouveauCopain) {
+        if (copains.size() < MAX_COPAINS && !copains.contains(nouveauCopain)) {
+            copains.add(nouveauCopain);
+            return true;
+        }
+        return false; 
+    }
+
+    public boolean supprimerCopain(Personne copainASupprimer) {
+        return copains.remove(copainASupprimer); 
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(prenom).append(" ").append(nom.toUpperCase());
+
+        if (copains.isEmpty()) {
+            sb.append(" et ses copains ()");
+        } else {
+            sb.append(" et ses copains (");
+            for (int i = 0; i < copains.size(); i++) {
+                sb.append(copains.get(i).getPrenom()).append(" ").append(copains.get(i).getNom().toUpperCase());
+                if (i < copains.size() - 1) {
+                    sb.append(", ");
+                }
+            }
+            sb.append(")");
+        }
+
+        return sb.toString();
+    }
 }
